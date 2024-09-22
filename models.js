e260bbbd9a480d50d8d8a27e26844072c1448b74
agush44 <magustinabenitez@gmail.com>
2024-09-22 17:51:01 -0300

    add function addUser

--- models.js
+++ models.js
@@ -1,4 +1,4 @@
-import { existsSync, read, readFileSync, write, writeFileSync } from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { randomUUID, createHash } from "node:crypto";
 import dotenv from "dotenv";
 import { handleError } from "./utils/handleError.js";
@@ -43,25 +43,57 @@ const getUserByID = (id) => {
   }
 };
 
-const res = getUserByID();
-console.log(res);
-
-/*
-//Recibe un objeto con toda la data para el nuevo usuario.
-//Valida que estén los datos mínimos para añadir un usuario.
-//Valida que el nombre sea un string
-//Valida que el apellido sea un string
-//Valida que el mail sea un string y que no se repita
-//Hashea la contraseña antes de registrar al usuario
 const addUser = (userData) => {
   try {
-  } catch (error) {}
+    const { name, lastName, email, password } = userData;
+
+    if (!name || !lastName || !email || !password) {
+      throw new Error("Missing data. All fields are required.");
+    }
+
+    if (
+      typeof name !== "string" ||
+      typeof lastName !== "string" ||
+      typeof email !== "string"
+    ) {
+      throw new Error("Invalid data: name, last name and mail must be strings");
+    }
+
+    const users = getUsers(PATH_FILE_USER);
+
+    const emailExists = users.some((user) => user.email === email);
+
+    if (emailExists) {
+      throw new Error("Invalid data: email is already registered");
+    }
+
+    const hash = createHash("sha256").update(password).digest("hex");
+
+    const newUser = {
+      id: randomUUID(),
+      name,
+      lastName,
+      email,
+      password: hash,
+      isLoggedIn: false,
+      createdAt: new Date().toISOString(),
+    };
+
+    users.push(newUser);
+    writeFileSync(PATH_FILE_USER, JSON.stringify(users));
+
+    return newUser;
+  } catch (error) {
+    const objError = handleError(error, PATH_FILE_ERROR);
+    return objError;
+  }
 };
 
+/*
 //Todos los datos del usuario pueden ser modificados menos el ID
 //Si se modifica la pass debería ser nuevamente hasheada
 //Si se modifica el mail, validar que no exista
-const updateUser = (userData) => {
+const updateUser = (userData) => {  
   try {
   } catch (error) {}
 };
@@ -70,6 +102,7 @@ const deleteUser = (id) => {
   try {
   } catch (error) {}
 };
+
 */
 
-//export { getUsers, getUserByID, addUser, deleteUser };
+export { getUsers, getUserByID, addUser, deleteUser };
